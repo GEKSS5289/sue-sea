e061a879958d8ff6e00e15faf9f7809aba831de8
GEKSS5289 <496966859@qq.com>
2020-08-18 12:15:12 +0800

    日志收集

---
+++ src/main/java/com/sue/colletor/api/IndexController.java
@@ -0,0 +1,25 @@
+package com.sue.colletor.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sue
+ * @date 2020/8/18 11:13
+ */
+
+@RestController
+@Slf4j
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+
+        return "idx";
+    }
+}
